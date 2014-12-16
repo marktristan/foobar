3b6b0683e5464307d797d5f78d372591762fbe3d
Mark Tristan Victorio <mt.victorio@dot.ph>
2014-12-16 17:06:00 +0800

    Fresh install with bootstrap

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+
+<head>
+
+  {{-- Basic page needs --}}
+  <meta charset="utf-8" />
+  <title>
+    @section('title')
+      My Project
+    @show
+  </title>
+
+  {{-- Mobile specific metas --}}
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  {{-- CSS --}}
+  {{ HTML::style('css/bootstrap.min.css') }}
+  {{ HTML::style('css/bootstrap-theme.css') }}
+
+  <style>
+    @section('styles')
+      body {
+        padding-top: 70px;
+      }
+    @show
+  </style>
+
+  {{-- HTML5 shim, for IE6-8 support of HTML5 elements --}}
+  <!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+
+<body>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
+  {{-- Javascripts --}}
+  {{ HTML::script('js/jquery-1.11.1.min.js') }}
+  {{ HTML::script('js/bootstrap.min.js') }}
+
+</body>
+
+</html>
