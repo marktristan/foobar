2570cc31b3ed2f24f3aae45fc3db10e0c4e7a6fc
Mark Tristan Victorio <mt.victorio@dot.ph>
2014-12-22 13:41:42 +0800

    Added date

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <hr>
 <footer>
-  <p>&copy; Company Copyright 2014 - Visit my repository at <a href="https://github.com/marktristan/foobar">github.com/marktristan/foobar</a></p>
+  <p>&copy; Company Copyright 2014 - Visit my repository at <a href="https://github.com/marktristan/foobar">github.com/marktristan/foobar</a> <span class="pull-right">{{{ date('F d, Y h:i a') }}}</span></p>
 </footer>
