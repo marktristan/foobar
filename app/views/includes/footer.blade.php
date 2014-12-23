fcb44a7ed8e89547abab4943cd40678b98d0f739
Mark Tristan Victorio <mt.victorio@dot.ph>
2014-12-23 17:42:02 +0800

    Removed pjax from other outside links

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <hr>
 <footer>
-  <small>&copy; Company Copyright 2014 - Visit my repository at <a href="https://github.com/marktristan/foobar">github.com/marktristan/foobar</a> <span class="pull-right"><a data-pjax="#main" href="{{ url('/') }}">Home</a> | <a href="#">Contact</a> | <a data-pjax="#main" href="{{ url('about') }}">About</a> - {{{ date('F d, Y h:i a') }}}</span></small>
+  <small>&copy; Company Copyright 2014 - {{{ date('F d, Y h:i a') }}} <span class="pull-right"><a href="{{ url('/') }}">Home</a> | <a href="#">Contact</a> | <a data-pjax="#main" href="{{ url('about') }}">About</a></span></small>
 </footer>
--- app/views/includes/headernav.blade.php
+++ app/views/includes/headernav.blade.php
@@ -1,19 +1,18 @@
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a data-pjax="#main" class="navbar-brand" href="{{ url('/') }}">Project Name</a>
+      <a class="navbar-brand" href="{{ url('/') }}">Project Name</a>
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Active Link</a></li>
+        <li {{ Request::is('/') ? 'class="active"' : '' }}><a href="{{ url('/') }}">Home</a></li>
         <li><a href="#">Another Link</a></li>
-        <li><a href="{{ url('/') }}">Non-pjax Link</a></li>
       </ul>
       <form class="navbar-form navbar-right" role="form" method="post" action="{{ url('/') }}">
         <div class="form-group">
@@ -23,7 +22,7 @@
           <input type="password" placeholder="Password" class="form-control" name="password">
         </div>
         <button type="button" class="btn btn-primary">Sign in</button> <!-- Replace button to submit if it will use as login -->
-        <a data-pjax="#main" class="btn btn-default" href="{{ url('account/create') }}">Register</a>
+        <a class="btn btn-default" href="{{ url('account/create') }}">Register</a>
       </form>
     </div><!--/.navbar-collapse -->
   </div>
